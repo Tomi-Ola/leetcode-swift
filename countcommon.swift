560ff6238f72d61bb63874a6587c2da04fd319fe
christianolay <jameywindsor@gmail.com>
2024-08-22 12:14:30 -0500

    code

---
+++ countcommon.swift
@@ -0,0 +1,17 @@
+let arr = ["cool", "lock", "cook"]
+var freq = [Int](repeating: 0, count: 26)
+var result = [Character]()
+
+for i in 0..<arr.count {
+    for ch in arr[i] {
+        let index = Int(ch.asciiValue! - Character("a").asciiValue!)
+        freq[index] = min(i + 1, freq[index] + 1)
+    }
+}
+
+for i in 0..<freq.count {
+    if freq[i] == arr.count {
+        result.append(Character(UnicodeScalar(i + Int(Character("a").asciiValue!))!))
+    }
+}
+print(result)
